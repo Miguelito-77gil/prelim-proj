57b233a21eec25124067d5cad8dc30acfbb35e36
Miguelito-77gil <antonioraze777@gmail.com>
2026-08-10 16:08:05 +0800

    Add files via upload

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="border-t border-gray-200 bg-transparent dark:border-gray-800 dark:bg-transparent">
-      <div className="mx-auto flex max-w-6xl justify-center px-4 py-5 text-center text-xs text-gray-800 sm:px-6 sm:py-6 sm:text-sm dark:text-gray-400">
+      <div className="mx-auto flex max-w-6xl justify-center px-4 py-5 text-center text-xs text-gray-950 sm:px-6 sm:py-6 sm:text-sm dark:text-gray-400">
         © 2026 Miguel. All rights reserved.
       </div>
     </footer>
