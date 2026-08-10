6fe2b5118c27782ae29abb8d96a2831feb60c4d5
Miguelito-77gil <antonioraze777@gmail.com>
2026-08-10 16:43:33 +0800

    Add files via upload

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,26 +1,10 @@
 import Navbar from "@/components/common/Navbar";
 import Footer from "@/components/common/Footer";
 import { Outlet } from "react-router-dom";
-import backgroundImage from "@/assets/mainbg.jpg";
 
 function MainLayout() {
   return (
     <div className="relative min-h-screen overflow-hidden">
-
-      {/* Whole Website Background */}
-      <div
-        className="pointer-events-none fixed inset-0 -z-20 scale-105 bg-cover bg-center bg-no-repeat blur-md"
-        style={{
-          backgroundImage: `url(${backgroundImage})`,
-        }}
-        aria-hidden="true"
-      />
-
-      {/* Light / Dark Overlay */}
-      <div
-        className="pointer-events-none fixed inset-0 -z-10 bg-white/60 dark:bg-black/50"
-        aria-hidden="true"
-      />
 
       {/* Header */}
      <header className="relative z-10 mx-auto max-w-6xl bg-transparent px-5 sm:px-8">
